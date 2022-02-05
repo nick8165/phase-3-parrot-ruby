ca6cb69b9e90c2d94786261a24c8d4a2ff99693d
nicholas shawn freeman <nick_freeman65@yahoo.com>
2022-02-05 15:43:19 -0600

    commit tree 666cb8615d7fbb939def56ca6947514ee042dc07

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,11 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+def parrot(phrase = "Squawk!")
+    if phrase == "Squawk!" 
+        puts phrase
+        phrase
+    else 
+        puts phrase
+        phrase
+    end
+end
